7687c74b9aa064be423803c761f7500f25e00e16
janhavi7126 <87857897+janhavi7126@users.noreply.github.com>
2022-12-08 11:41:13 +0530

    Update Is A Prime Number.java

--- Basic/Is A Prime Number.java
+++ Basic/Is A Prime Number.java
@@ -1,1 +1,29 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int t = scn.nextInt();
 
+      for(int a = 1; a<=t ; a++){
+        int n = scn.nextInt();
+        int factors = 0;
+
+        for(int i = 1; i*i<= n; i++){
+
+          if(n%i==0){
+            factors++;
+          }
+        }
+        if(factors == 1){
+          System.out.println("prime");
+        }
+        else{
+          System.out.println("not prime");
+        }
+
+      }
+  
+   }
+  }
