9b01364295ccc8bca843bfd26b5345b5c07a7a70
janhavi7126 <87857897+janhavi7126@users.noreply.github.com>
2022-12-15 19:38:48 +0530

    Update Main.java

--- Basic/Rotate A Number/Main.java
+++ Basic/Rotate A Number/Main.java
@@ -6,8 +6,8 @@ import java.util.*;
      Scanner scn = new Scanner(System.in);
      long n = scn.nextInt();
      long k = scn.nextInt();
-     long number = n;
-     long number2 = n;
+     long number = n    //23456
+     long number2 = n;  //23456
      int noOfDigits = 0;
      int sum =0;
 
@@ -16,20 +16,20 @@ import java.util.*;
         n = n/10;     
         noOfDigits++; 
     }
-    int digits = (int)Math.pow(10,noOfDigits -1);
+    int digits = (int)Math.pow(10,noOfDigits -1); // 10^4 
 
-          k = k  % noOfDigits  ;
+          k = k  % noOfDigits  ; // -43 % 5 = -3
            if(k<0)
            {                                       
-            k = noOfDigits + k  ;
+            k = noOfDigits + k  ;   -3 + 5 = 2
            }
       
 
-    for (int i = 0; i<k ; i++) {
-  
-    long r = number2%10;
-    number2 = number2/10;
-    number2 = number2 + r * digits ;
+    for (int i = 0; i<k ; i++) { //k=2       
+                                 // 0  1    
+    long r = number2%10;         // 6   5  
+    number2 = number2/10;        //2345   6234
+    number2 = number2 + r * digits ;   //2345+ 60000 = 62345      || 6234 + 50000=56234
 
     }  
 
