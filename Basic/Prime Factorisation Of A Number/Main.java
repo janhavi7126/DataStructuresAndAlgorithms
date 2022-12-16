a4a71bbd6820ed98b808f83b398ce3e0cc0544e9
janhavi7126 <87857897+janhavi7126@users.noreply.github.com>
2022-12-16 14:26:22 +0530

    Update Main.java

--- Basic/Prime Factorisation Of A Number/Main.java
+++ Basic/Prime Factorisation Of A Number/Main.java
@@ -6,14 +6,18 @@ public static void main(String[] args) {
   // write your code here
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
-
-	for (int i=2;i<=n;i++){
+  
+	for (int i=2;i*i<=n;i++){
 	         while (n%i == 0){
 	             System.out.print(i + " ");
 	             n = n/i;
 	            
 	         } 
 	     } 
+
+		 if(n != 1){
+			System.out.print(n);	 
+		 }
   
  }
 }
